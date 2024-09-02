1d18e3e06e473e8574724ebedad8b06310a6e1af
whykalashwhy <kalashbhaiya@gmail.com>
2024-09-02 18:34:41 -0400

    Deploy Next.js static site

--- my-next-app/next.config.mjs
+++ my-next-app/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Export as a static site
+    images: {
+      unoptimized: true, // Disable image optimization for GitHub Pages
+    },
+    basePath: '/track',
+    assetPrefix: '/track/', 
+  };
+  
+  export default nextConfig;
+  

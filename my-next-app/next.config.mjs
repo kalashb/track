45474425524c071dad1f8eb7aa616648d05d1d42
whykalashwhy <kalashbhaiya@gmail.com>
2024-09-02 18:28:54 -0400

    reverted

--- my-next-app/next.config.mjs
+++ my-next-app/next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/track', // Replace 'trac' with your repository name
-    assetPrefix: '/track/' // Same as above
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
